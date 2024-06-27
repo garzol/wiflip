7b564e14e5e77d6d0dd21c518480e7d6b6539027
garzol <dubophil@free.fr>
2024-06-27 14:39:48 +0200

    first biversion gorrlieb/recel

--- wiflip.py
+++ wiflip.py
@@ -53,13 +53,60 @@ class MainForm(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         #print sys.getdefaultencoding()
-        self.setWindowTitle("pipotage")
+
+
+        self.game_type = "Recel"
+        #self.game_type = "Gottlieb"
+
 
         self.ui = Ui_MainWindow()
         
 
+        
+        
+
         self.ui.setupUi(self)
+        self.setWindowTitle("WiFlip "+ self.game_type)
+
+        
+        
+        if self.game_type == "Recel":
+            DP = 92
+            BY = 69
+            BX = 10
+
+            self.setupmatchleds()
+            self.ui.label.setStyleSheet("background-image: url(images/1x/fair_fight_480.png);")
+            self.ui.wplayer1.setGeometry(QtCore.QRect(200, 350, 220, 38))
+            self.ui.wplayer2.setGeometry(QtCore.QRect(200, 350, 220, 38))
+            self.ui.wplayer3.setGeometry(QtCore.QRect(200, 350, 220, 38))
+            self.ui.wplayer4.setGeometry(QtCore.QRect(200, 350, 220, 38))
+            self.ui.ballinplay.setGeometry(QtCore.QRect(200, 350, 272//6, 38))
+            self.ui.credit.setGeometry(QtCore.QRect(200, 350, 272//6, 38))
+            self.ui.wplayer1.move(QPoint(BX,BY))
+            self.ui.wplayer2.move(QPoint(BX,BY+DP))
+            self.ui.wplayer3.move(QPoint(BX,BY+2*DP-4))
+            self.ui.wplayer4.move(QPoint(BX,BY+3*DP-7))
+            self.ui.credit.move(QPoint(50,15))
+            self.ui.ballinplay.move(QPoint(64,405))  #ballinplay is actually freeplay here
+            self.setupballinplay()
+            for i in range(5):
+                self.bip[i].setVisible (True)
+                self.bipt[i].setVisible(True)
+            self.bip[5].setVisible (False)
+            self.bipt[5].setVisible(False)
+            self.tiltIndicator.setVisible(True)
+        
+        elif self.game_type == "Gottlieb":   
+            self.ui.label.setStyleSheet("background-image: url(images/1x/pinballbell480.png);")
+            
+        if self.game_type == "Gottlieb":
+            self.swmRows, self.swmCols = (8, 5)
+        else:
+            self.swmRows, self.swmCols = (10, 4)
+
         self.setupleds()
+            
         self.msg68 = ""
         self.msg83 = ""
         
@@ -126,6 +173,10 @@ class MainForm(QtWidgets.QMainWindow):
         #dump button
         self.ui.pushButton_2.clicked.connect(self.send_dump)
  
+ 
+        #nvrdump button
+        self.ui.pushButton_R_dump.clicked.connect(self.send_nvrdump)
+
         #gpdump button
         self.ui.pushButton_4.clicked.connect(self.send_gpdump)
  
@@ -183,6 +234,14 @@ class MainForm(QtWidgets.QMainWindow):
         except:
             pass
         
+        
+    def send_nvrdump(self): 
+        self.linenb = 0
+        print("send command R")
+        try:
+            self.thread.sock.send(b' R')
+        except:
+            pass
         
     def send_gpdump(self): 
         self.linenb = 0
@@ -373,6 +432,16 @@ class MainForm(QtWidgets.QMainWindow):
             self.write2Console(f"{self.linenb:03d} {romaddr:03X} {romdata:02X}\t{ramaddr:03X} {ramdata:02X}\t  rst={rststate} rw={rwstate} wio={wiostate} bgt={bagotti}\r\n", insertMode=True)
             #print("typ=3", str(data))
         #print(str(data), len(data), typ)
+        elif typ == 82:    #R
+            print("Dump RAM in progress")
+            self.write2Console(f"NVRAM Current\r\n", insertMode=True)
+            for r in range(16):
+                self.write2Console(f"{r:02X}\t")
+                for l in range(8):
+                    self.write2Console(f"{data[r*8+l]:02X} ", insertMode=True)
+                self.write2Console(f"\r\n", insertMode=True)
+                    
+            
         elif typ == 71:   #G
             # 8b ROM data
             # 8b RAM data
@@ -411,20 +480,85 @@ class MainForm(QtWidgets.QMainWindow):
                 elif dspBstate == 1 and self.lastBstate == 0:
                     self.ui.label_DBState.setPixmap(QtGui.QPixmap(":/x/ledon.png"))
                 self.lastBstate = dspBstate
+                
+                if   self.game_type == "Gottlieb":   
+                    ballinplay = data[1:2]
+                    tableau1   = data[2:5]
+                    freeplay   = data[5:6]
+                    tableau2   = data[6:]
+                else:  #Recel case
+                    ballinplay = data[9:7:-1]     #tilt/game over/ball in play | match number
+                    tableau1   = data[7:3:-1]+b'0' #data[8] contains the dots state
+                    freeplay   = data[1::-1]      #freeplay|extra ball, here
+                    tableau2   = data[-7::]  +b'0' #data[-6] contains dots
                     
-                ballinplay = data[1:2]
-                tableau1   = data[2:5]
-                freeplay   = data[5:6]
-                tableau2   = data[6:]
                 aff1 = [self.ui.lcd1_6, self.ui.lcd1_5, self.ui.lcd1_4, self.ui.lcd1_3, self.ui.lcd1_2, self.ui.lcd1_1]
                 aff2 = [self.ui.lcd2_6, self.ui.lcd2_5, self.ui.lcd2_4, self.ui.lcd2_3, self.ui.lcd2_2, self.ui.lcd2_1]
                 aff5 = [self.ui.lcd5_2, self.ui.lcd5_1]
                 aff6 = [self.ui.lcd6_2, self.ui.lcd6_1]
-                
-                afflcdi(aff1, data[2:])
-                afflcdi(aff2, data[6:])
-                afflcdi(aff5, data[1:])
-                afflcdi(aff6, data[5:])
+
+                aff3 = [self.ui.lcd3_6, self.ui.lcd3_5, self.ui.lcd3_4, self.ui.lcd3_3, self.ui.lcd3_2, self.ui.lcd3_1]
+                aff4 = [self.ui.lcd4_6, self.ui.lcd4_5, self.ui.lcd4_4, self.ui.lcd4_3, self.ui.lcd4_2, self.ui.lcd4_1]
+
+                if   self.game_type == "Gottlieb":                   
+                    afflcdi(aff1, data[2:])
+                    afflcdi(aff2, data[6:])
+                    afflcdi(aff5, data[1:])
+                    afflcdi(aff6, data[5:])
+                else:
+                    #player 1
+                    data2=[((data[4]&0x0F)<<4)+((data[4]&0xF0)>>4),
+                           ((data[3]&0x0F)<<4)+((data[3]&0xF0)>>4),
+                           ((data[2]&0x0F)<<4)+0x0F]
+                    
+                    afflcdi(aff1, data2)   #tableau 1
+
+                    #player2                    
+                    data2=[((data[8]&0x0F)<<4)+((data[8]&0xF0)>>4),
+                           ((data[7]&0x0F)<<4)+((data[7]&0xF0)>>4),
+                           ((data[6]&0x0F)<<4)+0x0F]
+                    
+                    
+                    afflcdi(aff2, data2)     #tableau 2
+                    
+                    #afflcdi(aff5, data[5:])    #match number
+                    afflcdi(aff6, data[1:])    #freeplay/EB
+
+                    #match number
+                    for i in range(10):
+                        #print(i,(~data[5]&0x0F),data[5])
+                        if i==(~data[5]&0x0F):
+                            self.mled[i].setPixmap(QtGui.QPixmap(":/x/ledon.png"))
+                        else:
+                            self.mled[i].setPixmap(QtGui.QPixmap(":/x/ledgrey.png"))
+
+                    #ball in play + game over
+                    bstat = (data[5]&0x70)>>4
+                    bstat = bstat ^ 0b0111
+                    #print(f"{bstat:04b}   { ((data[5]&0x70))>>4:04b}")
+                    for i in range(5):
+                        if i==bstat:
+                            self.bip[i].setVisible (True)
+                            self.bipt[i].setVisible(True)
+                        else:
+                            self.bip[i].setVisible (False)
+                            self.bipt[i].setVisible(False)
+                    
+                    if bstat== 7:
+                        self.bip[5].setVisible (True)
+                        self.bipt[5].setVisible(True)
+                    else:
+                        self.bip[5].setVisible (False)
+                        self.bipt[5].setVisible(False)
+                        
+                                
+                    #tilt
+                    if ((~data[5]&0x80)):
+                        self.tiltIndicator.setVisible(True)
+                    else:
+                        self.tiltIndicator.setVisible(False)
+                        
+
                     
                 x = fromBcd2Int(tableau1)
                 if x == -1:
@@ -454,14 +588,22 @@ class MainForm(QtWidgets.QMainWindow):
                     self.ui.label_DBState.setPixmap(QtGui.QPixmap(":/x/ledon.png"))
                 self.lastBstate = dspBstate
 
-                tableau3   = data[2:5]
+                if   self.game_type == "Gottlieb":   
+                    tableau3   = data[2:5]
+                    tableau4   = data[6:]
+                else:
+                           
+                    tableau3   = data[-7::]    + b'0'
+                    tableau4   = data[7:2:-1]  + b'0'
+
                 x = fromBcd2Int(tableau3)
                 if x == -1:
                     self.ui.lcdNumber_3.setDigitCount(0)
                 else:
                     self.ui.lcdNumber_3.setDigitCount(6)
                     self.ui.lcdNumber_3.setProperty("value", x)
-                tableau4   = data[6:]
+                    
+                    
                 x = fromBcd2Int(tableau4)
                 if x == -1:
                     self.ui.lcdNumber_4.setDigitCount(0)
@@ -471,8 +613,28 @@ class MainForm(QtWidgets.QMainWindow):
                     
                 aff3 = [self.ui.lcd3_6, self.ui.lcd3_5, self.ui.lcd3_4, self.ui.lcd3_3, self.ui.lcd3_2, self.ui.lcd3_1]
                 aff4 = [self.ui.lcd4_6, self.ui.lcd4_5, self.ui.lcd4_4, self.ui.lcd4_3, self.ui.lcd4_2, self.ui.lcd4_1]
-                afflcdi(aff3, data[2:])
-                afflcdi(aff4, data[6:])
+                aff5 = [self.ui.lcd5_2, self.ui.lcd5_1]  #credits
+
+                if   self.game_type == "Gottlieb":                                  
+                    afflcdi(aff3, data[2:])
+                    afflcdi(aff4, data[6:])
+                else:
+                    #player 4
+                    data2=[((data[4]&0x0F)<<4)+((data[4]&0xF0)>>4),
+                           ((data[3]&0x0F)<<4)+((data[3]&0xF0)>>4),
+                           ((data[2]&0x0F)<<4)+0x0F]
+                           
+                    afflcdi(aff4, data2)   #tableau 4
+                    
+                    #player3                    
+                    data2=[((data[8]&0x0F)<<4)+((data[8]&0xF0)>>4),
+                           ((data[7]&0x0F)<<4)+((data[7]&0xF0)>>4),
+                           ((data[6]&0x0F)<<4)+0x0F]
+                    
+                    afflcdi(aff3, data2)     #tableau 3
+                    
+                    afflcdi(aff5, data[1:])    #credits
+
                 
                     
             elif typ == 67:  #'C'
@@ -634,16 +796,21 @@ class MainForm(QtWidgets.QMainWindow):
                         
                 
             elif typ == 83:  #'S'
-                #print(str(data))
-                #self.write2Console(f"switches:\n{data[0]:08b}\n{data[1]:08b}\n{data[2]:08b}\n{data[3]:08b}\n{data[3]:08b}", insertMode=False)
+                #print(len(data),"\t",str(data))
+                rows, cols = self.swmRows, self.swmCols  #(8,5) or (10,4)  
+
+                #self.write2Console(f"switches:\n{data[0]:08b}\n{data[1]:08b}\n{data[2]:08b}\n{data[3]:08b}\n{data[4]:08b}", insertMode=False)
                 self.msg83 = f"switches:\n{data[0]:08b}\n{data[1]:08b}\n{data[2]:08b}\n{data[3]:08b}\n{data[4]:08b}"
-                for i in range(8):
-                    for j in range(5):
-                        #print(i,j,data[j],data[j]&(1<<i))
-                        if data[j]&(1<<i):
-                            self.led[i][j].setPixmap(QtGui.QPixmap(":/x/ledon.png"))
-                        else:
-                            self.led[i][j].setPixmap(QtGui.QPixmap(":/x/ledgrey.png"))
+                #print(self.msg83)
+                for i in range(rows*cols):
+                    bitnumber  = i%8
+                    bytenumber = i//8
+                    r = i//cols
+                    c = i%cols
+                    if data[bytenumber]&(1<<bitnumber):
+                        self.led[r][c].setPixmap(QtGui.QPixmap(":/x/ledon.png"))
+                    else:
+                        self.led[r][c].setPixmap(QtGui.QPixmap(":/x/ledgrey.png"))
                             
             elif typ == 68:  #'D'
                 #print(str(data))
@@ -679,24 +846,24 @@ class MainForm(QtWidgets.QMainWindow):
 
 
     def setupleds(self):
-        rows, cols = (8, 5)
-        self.led = [0]*8
+        rows, cols = self.swmRows, self.swmCols    
+        self.led = [0]*rows
         
-        for i in range(8):
+        for i in range(rows):
             rled=QtWidgets.QLabel(self.ui.groupBox)
             rled.setGeometry(QtCore.QRect(60+i*28, 90-28, 20, 20))
             rled.setText(f"S{i}")
             rled.setObjectName(f"sled_{i}")
             
-        for i in range(5):
+        for i in range(cols):
             rled=QtWidgets.QLabel(self.ui.groupBox)
             rled.setGeometry(QtCore.QRect(60-28, 90+i*36, 20, 20))
             rled.setText(f"R{i}")
             rled.setObjectName(f"rled_{i}")
             
-        for i in range(8):
-            self.led[i]=[0]*5
-            for j in range(5):
+        for i in range(rows):
+            self.led[i]=[0]*cols
+            for j in range(cols):
                 self.led[i][j]=QtWidgets.QLabel(self.ui.groupBox)
                 self.led[i][j].setGeometry(QtCore.QRect(60+i*28, 90+j*36, 20, 20))
                 self.led[i][j].setText("")
@@ -712,6 +879,85 @@ class MainForm(QtWidgets.QMainWindow):
         #     for j in range(5):
         #         if i%2 and j%2:
         #             self.led[i][j].setPixmap(QtGui.QPixmap(":/x/ledon.png"))
+
+    def setupballinplay(self):
+        self.bip = [0]*6
+        self.bipt = [0]*6
+        for i in range(5):
+            self.bip[i]=QtWidgets.QLabel(self.ui.centralwidget)
+            self.bip[i].setGeometry(QtCore.QRect(300+i*30, 365, 20, 20))
+            self.bip[i].setPixmap(QtGui.QPixmap(":/x/ledyellow"))
+            self.bip[i].setScaledContents(True)
+            self.bip[i].setObjectName(f"bip{i}")
+            
+            self.bipt[i]=QtWidgets.QLabel(self.ui.centralwidget)
+            self.bipt[i].setGeometry(QtCore.QRect(300+i*30, 365, 20, 20))
+            self.bipt[i].setStyleSheet("QLabel { background-color : transparent; color : darkGreen; font-size: 14; font-weight: bold;}")
+            self.bipt[i].setText(f"  {i+1} ")
+            self.bipt[i].setScaledContents(True)
+            self.bipt[i].setObjectName(f"bipt{i}")
+
+        self.bip[5]=QtWidgets.QLabel(self.ui.centralwidget)
+        self.bip[5].setGeometry(QtCore.QRect(300, 365, 20, 20))
+        self.bip[5].setPixmap(QtGui.QPixmap(":/x/ledyellow"))
+        self.bip[5].setScaledContents(True)
+        self.bip[5].setObjectName(f"bip5")
+        
+        self.bipt[5]=QtWidgets.QLabel(self.ui.centralwidget)
+        self.bipt[5].setGeometry(QtCore.QRect(300, 365, 100, 20))
+        self.bipt[5].setStyleSheet("QLabel { background-color : orange; color : white; font-weight: bold;}")
+        self.bipt[5].setText(f"  GAME OVER")
+        self.bipt[5].setScaledContents(True)
+        self.bipt[5].setObjectName(f"bipt5")
+
+
+        self.tiltIndicator = QtWidgets.QLabel(self.ui.centralwidget)
+        self.tiltIndicator.setGeometry(QtCore.QRect(175, 425, 50, 20))
+        self.tiltIndicator.setStyleSheet("QLabel { background-color : orange; color : white; font-weight: bold;}")
+        self.tiltIndicator.setText(f" T I L T")
+        self.tiltIndicator.setScaledContents(True)
+
+    def setupmatchleds(self):
+        
+        self.mled  = [0]*10
+        self.mledt = [0]*10
+        
+        # for i in range(8):
+        #     rled=QtWidgets.QLabel(self.ui.groupBox)
+        #     rled.setGeometry(QtCore.QRect(60+i*28, 90-28, 20, 20))
+        #     rled.setText(f"S{i}")
+        #     rled.setObjectName(f"sled_{i}")
+            
+        ccenter = (419.0, 165.0)
+        cr      =  28.0
+
+        for i in range(10):
+            self.mledt[i]=QtWidgets.QLabel(self.ui.centralwidget)
+            xc = ccenter[0] + (cr+20)*math.cos(2.0*math.pi*i/10.0 + math.pi/2.0)
+            yc = ccenter[1] + (cr+20)*math.sin(2.0*math.pi*i/10.0 + math.pi/2.0)
+            xc = int(xc)
+            yc = int(yc)
+            self.mledt[i].setGeometry(QtCore.QRect(xc, yc, 20, 20))
+            self.mledt[i].setStyleSheet("QLabel { background-color : black; color : white; }")
+
+            self.mledt[i].setText(str(i)+"0")
+            self.mledt[i].setScaledContents(True)
+            self.mledt[i].setObjectName(f"matchledt_{i}")
+          
+        for i in range(10):
+            self.mled[i]=QtWidgets.QLabel(self.ui.centralwidget)
+            xc = ccenter[0] + cr*math.cos(2.0*math.pi*i/10.0 + math.pi/2.0)
+            yc = ccenter[1] + cr*math.sin(2.0*math.pi*i/10.0 + math.pi/2.0)
+            xc = int(xc)
+            yc = int(yc)
+            self.mled[i].setGeometry(QtCore.QRect(xc, yc, 20, 20))
+            self.mled[i].setText("")
+            self.mled[i].setPixmap(QtGui.QPixmap(":/x/ledgrey.png"))
+            self.mled[i].setScaledContents(True)
+            self.mled[i].setObjectName(f"matchled_{i}")
+
+
+
                     
 class Worker(QThread):
     output = pyqtSignal(bytearray, int)
@@ -808,6 +1054,8 @@ class Worker(QThread):
                 framesz = 5
             elif received == b'G':
                 framesz = 5
+            elif received == b'R':
+                framesz = 128
             elif received == b'P':
                 #print("P")
                 framesz = 32 #32
@@ -934,8 +1182,14 @@ def fromBcd2Int(barr):
     if nbf == 2*len(barr):
         ret = -1
     return ret
-        
+
+def swapint(v):
+    return ((v&0x0F)<<4)+((v&0xF0)>>4)      
+   
 def afflcdi(afftab, data):
+    #print(type(data))     => bytearray
+    #print(type(data[0]))  => int
+    
     for idx in range(len(afftab)):
         afftab[idx].setHexMode()
         if not idx%2:
